ff7bb835c5a06376b8e0eed7f0e555fbd6b9b72e
bhavik712 <baradbhavik41@gmail.com>
2023-08-04 12:39:28 +0530

    use of props & vif

---
+++ src/components/voting.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <!-- show the message based on age --using v-if dirv -->
+        <p v-if = " myAge < 18 "> you can vote after {{18 - myAge }}years</p>
+        <p v-else>You should vote in every election.</p>
+        
+    </div>
+</template>
+
+<script>
+export default{
+    name:'Voting',
+    props:['myAge']
+   
+}
+
+</script>
